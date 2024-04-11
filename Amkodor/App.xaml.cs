4a06528341052fba5f4f19f1ca9ab8feda89411a
Stecko Anton <lemonadetxt@gmail.com>
2024-04-11 13:54:54 +0300

    Add project files.

---
+++ Amkodor/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Amkodor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
